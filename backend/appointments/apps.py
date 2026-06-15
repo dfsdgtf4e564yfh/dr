04f0ab8f54a09560278e513ac1ad5822dba8a618
Mohammad <aria.shahi1997@gmail.com>
2026-06-15 14:52:57 +0330

    151

--- backend/appointments/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppointmentsConfig(AppConfig):
-    name = 'appointments'
-    verbose_name = 'نوبت‌ها'
-
-    def ready(self):
-        import appointments.signals
