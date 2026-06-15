677ac3a80fdf871d0fc9cd3f3cf7ae6260f3eda2
Mohammad <aria.shahi1997@gmail.com>
2026-06-15 13:31:00 +0330

    15615

--- backend/patients/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'national_id', 'phone', 'file_number', 'education', 'job', 'is_deleted', 'created_at']
-    search_fields = ['first_name', 'last_name', 'national_id', 'phone', 'file_number']
-    list_filter = ['is_deleted', 'education', 'gender']
