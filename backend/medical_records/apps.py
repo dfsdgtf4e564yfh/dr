b3b26f109714e0d06210aa7092565f8fb06ddff6
Mohammad <aria.shahi1997@gmail.com>
2026-06-19 19:03:23 +0330

    3242423442

--- backend/medical_records/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedicalRecordsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medical_records'
-
-    def ready(self):
-        import medical_records.signals
